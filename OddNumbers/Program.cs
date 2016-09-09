9d85bbbf925e62d195c3c60a19ab7423ef605ebb
dmeraz <darin.meraz@gmail.com>
2016-09-08 22:16:06 -0400

    OddNumbers

---
+++ OddNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter;
+            for (counter = 1; counter <= 99; counter += 2)
+            {
+                System.Console.WriteLine(counter);
+            } 
+        }
+    }
+}
